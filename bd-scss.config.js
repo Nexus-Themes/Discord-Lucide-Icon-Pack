35528f24e1f0fe63fc2295e1a6e39063067f66de
Riddim_GLiTCH <Riddim_GLiTCH@proton.me>
2025-02-04 15:19:04 +0100

    Initial commit - First set of Icons done.

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "Lucide Icon Pack",
+		author: "Nexus-Themes",
+		version: "1.0.0",
+		description: "a mini-theme that replaces all Discord Icons with Lucide ones.",
+		source: "https://github.com/Nexus-Themes/Lucide Icon Pack"
+	}
+}
